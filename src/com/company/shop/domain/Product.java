23ec9732675c4d12dd18e2b15ad39cba0cbab6f6
hykim <hykim22@gmail.com>
2026-05-14 16:21:57 +0900

    feat: 상품 기능 파일 생성

---
+++ src/com/company/shop/domain/Product.java
@@ -0,0 +1,17 @@
+/**
+ * 파일명: Product.java  <br>
+ * 설명:  PRODUCTS 테이블<br>
+ * 작성자: user <br>
+ * 작성일: 2026. 5. 14. <br>
+ * 버전 : 1.0
+ */
+package com.company.shop.domain;
+
+/**
+ * 
+ */
+public class Product {
+    private Long productId;
+    private String productName;
+    private int price;
+}
