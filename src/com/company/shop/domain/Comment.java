14d06044bdee00f3769d4ffd7a9c620ccd657d29
hykim <hykim22@gmail.com>
2026-05-14 17:58:46 +0900

    feat:	new file:   src/com/company/shop/domain/Board.java 	new file:   src/com/company/shop/domain/Comment.java 	new file:   src/com/company/shop/service/BoardService.java 	new file:   src/com/company/shop/service/CommentService.java

---
+++ src/com/company/shop/domain/Comment.java
@@ -0,0 +1,17 @@
+/**
+ * 파일명: Comment.java  <br>
+ * 설명:  COMMENTS 테이블 개념<br>
+ * 작성자: user <br>
+ * 작성일: 2026. 5. 14. <br>
+ * 버전 : 1.0
+ */
+package com.company.shop.domain;
+
+/**
+ * 
+ */
+public class Comment {
+    private Long commentId;
+    private Long boardId;
+    private String comment;
+}
