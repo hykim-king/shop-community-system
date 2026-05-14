8e072b19e2a9c5bf436427404dfaa3c66dd0a2e4
hykim <hykim22@gmail.com>
2026-05-14 16:19:21 +0900

    feat: 회원, 로그인 파일 추가# Please enter the commit message for your changes. Lines starting

---
+++ src/com/company/shop/domain/Member.java
@@ -0,0 +1,18 @@
+/**
+ * 파일명: Member.java  <br>
+ * 설명:  회원<br>
+ * 작성자: user <br>
+ * 작성일: 2026. 5. 14. <br>
+ * 버전 : 1.0
+ */
+package com.company.shop.domain;
+
+/**
+ * 
+ */
+public class Member {
+    private Long memberId;
+    private String loginId;
+    private String password;
+    private String name;
+}
